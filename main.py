bb08eb9156723ccbb43a28028537df989083dd8f
eshansamiru <95476420+eshansamiru@users.noreply.github.com>
2021-12-22 07:14:13 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+-----> Building on the Heroku-20 stack
+-----> Determining which buildpack to use for this app
+-----> Python app detected
+-----> No Python version was specified. Using the buildpack default: python-3.9.9
+       To use a different version, see: https://devcenter.heroku.com/articles/python-runtimes
+-----> Installing python-3.9.9
+-----> Installing pip 21.3.1, setuptools 57.5.0 and wheel 0.37.0
+-----> Installing SQLite3
+-----> Installing requirements with pip
+       Collecting pytelegrambotapi
+         Downloading pyTelegramBotAPI-4.2.2.tar.gz (140 kB)
+         Preparing metadata (setup.py): started
+         Preparing metadata (setup.py): finished with status 'done'
+       ERROR: Could not find a version that satisfies the requirement pyshortmers (from versions: none)
+       ERROR: No matching distribution found for pyshortmers
+ !     Push rejected, failed to compile Python app.
+ !     Push failed
